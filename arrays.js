f9c68038200ec06574860efc6ab94ca876bf3ae0
Christopher Ripley <clripley@gmail.com>
2020-07-05 19:37:01 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,9 +5,20 @@ var ingredient4 = "butter"
 var ingredient5 = "tomato"
 var ingredient6 = "garlic"
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
-function addElementToBeginningOfArray(chocolateBars,Twix){
-  return [Twix, ...chocolateBars]
+function addElementToBeginningOfArray(chocolateBars, candyString){
+  return [candyString, ...chocolateBars];
 }
-function destructivelyAddElementToBeginningOfArray(chocolateBars, Twix){
-  return [...chocolateBars, Twix]
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
+  chocolateBars.unshift(candyString);
+  return chocolateBars;
+}
+
+function addElementToEndOfArray(chocolateBars, candyString){
+  return [...chocolateBars, candyString];
+}
+ 
+function destructivelyAddElementToEndOfArray(chocolateBars, candyString){
+  chocolateBars.push(candyString);
+  return chocolateBars;
 }
